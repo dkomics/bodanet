3a5e20c8615b3040a84e1d9a4c77cd8bf1289cbf
dkomics <klay.py@gmail.com>
2025-11-12 16:58:51 +0300

    feat: add JWT authentication endpoints

--- bodanet/urls.py
+++ bodanet/urls.py
@@ -17,6 +17,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include   
 from django.http import JsonResponse
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
 
 def home(request):
     return JsonResponse({"message": "bodanet root"})
@@ -27,5 +31,8 @@ urlpatterns = [
     path('api/', include('core.urls')),
     path('api/', include('fare.urls')),
     path('api/', include('queueing.urls')),
+    # auth
+    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     
 ]
